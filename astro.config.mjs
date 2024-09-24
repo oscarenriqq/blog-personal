a5247f2b3fe02c5bd61f2963fe6c991a0c863740
Oscar Molinar <oscarmolinar96@gmail.com>
2024-09-24 13:32:10 -0500

    Add Alpine integration and blog page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    alpinejs()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'one-dark-pro'
     }
   },
-
 });
